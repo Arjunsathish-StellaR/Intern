f438ff1c76ce20c470d93b05a22ad2d9fac8f55c
Arjunsathish-StellaR <114048178+Arjunsathish-StellaR@users.noreply.github.com>
2023-05-26 11:49:24 +0530

    Update dut_test.py

--- test/dut_test.py
+++ test/dut_test.py
@@ -6,7 +6,7 @@ from cocotb.triggers import Timer
 async def dut_test(dut):
 	a = (0, 0, 1, 1)
 	b = (0, 1, 0, 1)
-	y = (0, 1, 1, 0)
+	y = (0, 1, 1, 1)
 	for i in range(4):
 		dut.a.value = a[i]
 		dut.b.value = b[i]
